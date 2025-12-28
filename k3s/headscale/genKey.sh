7dd61fe16fadfa45fb8d39ad88be56f443045bdd
정석호 <creaticoding@gmail.com>
2025-12-29 08:44:52 +0900

    feat: genKey.sh에 사용자 인자 추가

--- k3s/headscale/genKey.sh
+++ k3s/headscale/genKey.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 HEADSCALE_NAME=$(kubectl get pods -n headscale -o jsonpath='{.items[0].metadata.name}')
+USER=$1
 
-kubectl exec -it -n headscale $HEADSCALE_NAME -- headscale preauthkeys create --reusable --expiration 24h
+if [ -z "$USER" ]; then
+  echo "Usage: $0 <user>"
+  exit 1
+fi
+
+kubectl exec -it -n headscale $HEADSCALE_NAME -- headscale preauthkeys create --user $USER --reusable --expiration 24h
